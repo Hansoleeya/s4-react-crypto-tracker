0c668d4a37072a3b100070ab776feb897ba8e31b
Hansoleeya <yang11hs@naver.com>
2022-05-27 11:01:47 +0900

    Line Charts

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,4 +1,29 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinToday } from "../api";
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+interface IPriceProps{
+    coinId : string;
+}
+
 function Price(){
+    const coinId = useOutletContext<IPriceProps>();
+    const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId.coinId],
+        () => fetchCoinToday(coinId.coinId!),
+        {
+            //refetchInterval: 10000,
+        });
     return <h1>price</h1>
 }
 
